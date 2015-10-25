996728303755ba94ad4684630555680ab33716d9
taapesh <starmansix@gmail.com>
2015-10-24 23:19:57 -0500

    Added url for get tables by address

--- rest_api_proj/urls.py
+++ rest_api_proj/urls.py
@@ -9,5 +9,6 @@ urlpatterns += [
     url(r'^tables/$', views.table_list),
     url(r'^tables/(?P<ownerId>[0-9]+)/$', views.table_detail),
     url(r'^servertables/', views.get_server_tables),
-    url(r'^makerequest/(?P<ownerId>[0-9]+)/$', views.make_table_request)
+    url(r'^makerequest/(?P<ownerId>[0-9]+)/$', views.make_table_request),
+    url(r'^gettableaddr/', views.get_table_by_addr)
 ]
