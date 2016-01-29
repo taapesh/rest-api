63a3964a80cb07fa93200796139037fdca96a386
taapesh <starmansix@gmail.com>
2016-01-29 08:26:06 -0600

    Testing api locally

--- rest_api_proj/settings.py
+++ rest_api_proj/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-DATABASES['default'] = dj_database_url.config()
+#DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 ALLOWED_HOSTS = ['*']
