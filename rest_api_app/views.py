64b1077269a8add4c239704c0ab2ec21f3e8fbe4
taapesh <starmansix@gmail.com>
2016-01-29 06:49:38 -0600

    Remove unused views

--- rest_api_app/views.py
+++ rest_api_app/views.py
@@ -23,8 +23,7 @@ def create_or_join_table(request):
             user.save()
             return Response({
                 "message": "table created",
-                "table_id": serializer.data.get('id'),
-                "party_size": serializer.data.get('party_size')
+                "table_id": serializer.data.get('id')
             }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -41,7 +40,6 @@ def create_or_join_table(request):
         "table_id": table.id,
         "party_size": table.party_size
     }, status=status.HTTP_200_OK)
-
 
 @api_view(['POST'])
 #@authentication_classes([TokenAuthentication])
@@ -72,123 +70,4 @@ def get_users_at_table(request):
 
 def find_server():
     return
-
-
-
-
-@api_view(['GET', 'POST'])
-#@authentication_classes([TokenAuthentication])
-#@permission_classes([permissions.IsAuthenticated])
-def table_list(request):
-    """
-    List all tables, or create a new table.
-    """
-    if request.method == 'GET':
-        tables = Table.objects.all()
-        serializer = TableSerializer(tables, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = TableSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET', 'PUT', 'DELETE'])
-#@authentication_classes([TokenAuthentication])
-#@permission_classes([permissions.IsAuthenticated])
-def table_detail(request, ownerId):
-    """
-    Retrieve, update or delete a table.
-    """
-    try:
-        table = Table.objects.get(ownerId=ownerId)
-    except Table.DoesNotExist:
-        return Response({"error":"-1"}, status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = TableSerializer(table)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = TableSerializer(table, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        table.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-@api_view(['GET', 'POST'])
-#@authentication_classes([TokenAuthentication])
-#@permission_classes([permissions.IsAuthenticated])
-def get_table_by_addr(request):
-    try:
-        table = Table.objects.get(address=request.data.get('addr'), tableNum=request.data.get('tableNum'))
-    except Table.DoesNotExist:
-        return Response({"error":"-1"}, status=status.HTTP_404_NOT_FOUND)
-
-    serializer = TableSerializer(table)
-    return Response(serializer.data)
-
-
-@api_view(['GET', 'POST'])
-#@authentication_classes([TokenAuthentication])
-#@permission_classes([permissions.IsAuthenticated])
-def get_server_tables(request):
-    if (request.data.get('finished')):
-        tables = Table.objects.filter(serverId=request.data.get('serverId')).filter(isFinished=True)
-    elif (request.data.get('requested')):
-        tables = Table.objects.filter(serverId=request.data.get('serverId')).filter(requestMade=True)
-    else:
-        tables = Table.objects.filter(serverId=request.data.get('serverId'))
-
-    serializer = TableSerializer(tables, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET', 'POST'])
-#@authentication_classes([TokenAuthentication])
-#@permission_classes([permissions.IsAuthenticated])
-def leave_table(request):
-    user = User.objects.get(id=userId)
-
-    try:
-        table = Table.objects.get(ownerId=user.currentTableId)
-    except Table.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    table.partySize -= 1
-
-
-@api_view(['GET'])
-#@authentication_classes([TokenAuthentication])
-#@permission_classes([permissions.IsAuthenticated])
-def make_table_request(request):
-    try:
-        table = Table.objects.get(ownerId=ownerId);
-    except Table.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    table.requestMade = True
-    table.save()
-    return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-@api_view(['GET'])
-#@authentication_classes([TokenAuthentication])
-#@permission_classes([permissions.IsAuthenticated])
-def increase_party_count(request):
-    try:
-        table = Table.objects.get(ownerId=ownerId);
-    except Table.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    table.partySize += 1
-    table.save()
-    return Response(status=status.HTTP_204_NO_CONTENT)
 
--- rest_api_proj/urls.py
+++ rest_api_proj/urls.py
@@ -8,11 +8,6 @@ urlpatterns = patterns('',
 
 urlpatterns += [
     url(r'^admin/', admin.site.urls),
-    url(r'^tables/$', views.table_list),
-    url(r'^tables/(?P<ownerId>[0-9]+)/$', views.table_detail),
-    url(r'^servertables/', views.get_server_tables),
-    url(r'^makerequest/(?P<ownerId>[0-9]+)/$', views.make_table_request),
-    url(r'^gettableaddr/', views.get_table_by_addr),
 
     url(r'^createtable/', views.create_or_join_table),
     url(r'^alltables/', views.get_all_tables),
